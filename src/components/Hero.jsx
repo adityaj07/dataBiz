060c3080575b3cc7eda0a766eec69fbbc2641e77
Aditya Joshi <joshiaditya.aj07@gmail.com>
2023-05-29 13:06:18 +0530

    feat: Navbar, Hero complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   return (
-    <div className='text-white max-w-[1240px] m-auto px-4'>Ram</div>
-  )
-}
+    <div className="text-white">
+      <div className=" max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-1">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="text-4xl sm:text-6xl md:text-7xl font-bold md:py-6">
+          Grow with data.
+        </h1>
+        <div>
+          <p className="text-xl sm:text-2xl md:text-3xl">
+            Fast, flexible financing for
+            <TypeAnimation
+              sequence={["B2B", 2000, "B2C", 2000, "SaaS",2000]}
+              wrapper="span"
+              cursor={true}
+              repeat={Infinity}
+              style={{
+                fontSize: "1.875rem",
+                lineHeight: "2.25rem",
+                display: "inline-block",
+                marginLeft: "10px",
+                fontWeight:"bold"
+              }}
+            />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,73 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-  const handleNav = () => {
-    setNav(nav => !nav)
-  }
+  const handleNavToggle = () => {
+    setIsNavOpen((prevState) => !prevState);
+  };
 
   return (
-    <div className="max-w-[1240px] flex justify-between items-center h-24 px-4 m-auto text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">dataBiz.</h1>
+    <nav className="max-w-[1240px] flex justify-between items-center h-24 px-4 m-auto text-white">
+      <h1 className="text-3xl font-bold text-[#00df9a]">dataBiz.</h1>
       <ul className="hidden md:flex">
-        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">Home</li>
-        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">Company</li>
-        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">Resources</li>
-        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">About</li>
-        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">Contact</li>
+        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+          Home
+        </li>
+        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+          Company
+        </li>
+        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+          Resources
+        </li>
+        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+          About
+        </li>
+        <li className="text-xl m-4 cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+          Contact
+        </li>
       </ul>
 
-      <div onClick={handleNav} className="block md:hidden"> 
-        {!nav ? <RiCloseFill size={20} className="cursor-pointer"/> : <RiMenu3Fill size={20} className="cursor-pointer"/>}
+      <div onClick={handleNavToggle} className="block md:hidden">
+        {isNavOpen ? (
+          <RiCloseFill size={20} className="cursor-pointer" />
+        ) : (
+          <RiMenu3Fill size={20} className="cursor-pointer" />
+        )}
       </div>
 
       {/* MOBILE NAV   */}
-      <div className={!nav ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-600 h-full bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4 mt-[1.9rem]">
+      <div
+        className={`fixed left-0 top-0 w-[60%] border-r border-r-gray-600 h-full bg-[#000300] transition-transform duration-500 ${
+          isNavOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <h1 className="text-3xl font-bold text-[#00df9a] m-4 mt-[1.9rem]">
           dataBiz.
         </h1>
         <ul className="flex flex-col uppercase p-3">
-          <li className="m-4 border-b border-gray-700 text-xl cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">Home</li>
-          <li className="m-4 border-b border-gray-700 text-xl cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">Company</li>
-          <li className="m-4 border-b border-gray-700 text-xl cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">Resources</li>
-          <li className="m-4 border-b border-gray-700 text-xl cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">About</li>
-          <li className="m-4 text-xl cursor-pointer hover:text-[#00df9a] hover: ease-in-out duration-100">Contact</li>
+          <li className="m-4 border-b border-gray-700 text-xl cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+            Home
+          </li>
+          <li className="m-4 border-b border-gray-700 text-xl cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+            Company
+          </li>
+          <li className="m-4 border-b border-gray-700 text-xl cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+            Resources
+          </li>
+          <li className="m-4 border-b border-gray-700 text-xl cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+            About
+          </li>
+          <li className="m-4 text-xl cursor-pointer hover:text-[#00df9a] transition-colors duration-100">
+            Contact
+          </li>
         </ul>
       </div>
       {/* MOBILE NAV END  */}
-    </div>
+    </nav>
   );
 };
 
 export default Navbar;
+
