363d21f751c66f4f2af7d6c6dad47e8f89bb95f2
Aditya Joshi <joshiaditya.aj07@gmail.com>
2023-05-31 00:58:17 +0530

    feat: Added price cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+// Card.js
+import React from "react";
+import Button from "../components/Button";
+
+const Card = ({ user, price, offeredServices, image,btnClassName }) => {
+  const isDoubleUser = user === "Double User"; // Check if the card is for Double User
+
+  return (
+    <div
+      className={`shadow-xl rounded-lg flex flex-col p-4 my-16 hover:scale-105 duration-300 ${
+        isDoubleUser ? "scale-110 hover:scale-125 bg-gradient-to-r from-[#00fd9a] to-[#006941]" : ""
+      }`}
+    >
+      <img src={image} alt={user} className="w-20 mx-auto mt-[-3rem] bg-white" />
+      <h2 className="text-2xl font-bold text-center py-8 pb-3 mx-auto">{user}</h2>
+      <p className="text-4xl font-bold mx-auto">{price}</p>
+      <div className="text-center font-medium mt-3">
+        {Object.values(offeredServices).map((service, index) => (
+          <p key={index} className="py-2 border-b mx-8">{service}</p>
+        ))}
+      </div>
+      <Button
+        text="Start Trial"
+        className={`w-[145px] mx-auto my-8 hover:bg-black hover:text-[#00fd9a] transition-all duration-200 ${btnClassName}`}
+      />
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import Card from "./Card";
+import Single from "../assets/single.png";
+import Double from "../assets/double.png";
+import Triple from "../assets/triple.png";
 
 const Cards = () => {
-  return (
-    <div className='w-full py-[10rem] px-4 bg-white'></div>
-  )
-}
+  const singleUser = {
+    service1: "500 GB Storage",
+    service2: "1 Granted User",
+    service3: "Send up to 2 GB"
+  };
 
-export default Cards
+  const doubleUser = {
+    service1: "1100 GB Storage",
+    service2: "2 Granted Users",
+    service3: "Send up to 6 GB"
+  };
+
+  const tripleUser = {
+    service1: "1800 GB Storage",
+    service2: "3 Granted Users",
+    service3: "Send up to 12 GB"
+  };
+
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-16">
+        <Card
+          user="Single User"
+          price="$149"
+          offeredServices={singleUser}
+          image={Single}
+        />
+        <Card
+          user="Double User"
+          price="$299"
+          offeredServices={doubleUser}
+          image={Double}
+          btnClassName="bg-white hover:text-white"
+          
+        />
+        <Card
+          user="Triple User"
+          price="$499"
+          offeredServices={tripleUser}
+          image={Triple}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
